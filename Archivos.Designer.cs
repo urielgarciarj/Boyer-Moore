80745af410ce9c52a6d079e09a145d2c24593e19
Uriel <urielgarciarj@outlook.com>
2019-10-20 23:16:28 -0500

    Agregando

---
+++ Archivos.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace BoyerMoore_csharp
+{
+    partial class Archivos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.txtArchive = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSaveAsArchive = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.cbSensible = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(103, 35);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(644, 355);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(12, 35);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 1;
+            this.btnNew.Text = "Nuevo";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 76);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Abrir";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(13, 121);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Location = new System.Drawing.Point(13, 169);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(84, 23);
+            this.btnSaveAs.TabIndex = 4;
+            this.btnSaveAs.Text = "Guardar Como";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(13, 214);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(12, 404);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(79, 13);
+            this.label.TabIndex = 6;
+            this.label.Text = "Archivo a Abrir:";
+            // 
+            // txtArchive
+            // 
+            this.txtArchive.Location = new System.Drawing.Point(98, 401);
+            this.txtArchive.Name = "txtArchive";
+            this.txtArchive.Size = new System.Drawing.Size(113, 20);
+            this.txtArchive.TabIndex = 7;
+            this.txtArchive.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtArchive_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Guardar como:";
+            // 
+            // txtSaveAsArchive
+            // 
+            this.txtSaveAsArchive.Location = new System.Drawing.Point(317, 404);
+            this.txtSaveAsArchive.Name = "txtSaveAsArchive";
+            this.txtSaveAsArchive.Size = new System.Drawing.Size(100, 20);
+            this.txtSaveAsArchive.TabIndex = 9;
+            this.txtSaveAsArchive.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSaveAsArchive_KeyDown);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(15, 268);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Busqueda";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(467, 404);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Palabra a buscar:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(564, 404);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 12;
+            this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyDown);
+            // 
+            // cbSensible
+            // 
+            this.cbSensible.AutoSize = true;
+            this.cbSensible.Location = new System.Drawing.Point(671, 406);
+            this.cbSensible.Name = "cbSensible";
+            this.cbSensible.Size = new System.Drawing.Size(66, 17);
+            this.cbSensible.TabIndex = 13;
+            this.cbSensible.Text = "Sensible";
+            this.cbSensible.UseVisualStyleBackColor = true;
+            // 
+            // Archivos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbSensible);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSaveAsArchive);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtArchive);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.richTextBox);
+            this.Name = "Archivos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Archivos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox txtArchive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSaveAsArchive;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.CheckBox cbSensible;
+    }
+}
+
---
+++ Archivos.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BoyerMoore_csharp
+{
+    public partial class Archivos : Form
+    {
+        public Archivos()
+        {
+            InitializeComponent();
+            richTextBox.Hide();
+            label.Hide();
+            txtArchive.Hide();
+            btnSave.Hide();
+            btnSaveAs.Hide();
+            label2.Hide();
+            txtSaveAsArchive.Hide();
+            label3.Hide();
+            txtSearch.Hide();
+            cbSensible.Hide();
+        }
+
+        public static class GlobalData
+        {
+            public static int pattern = 0; //Largo del patron
+            public static int text = 0; //largo del texto
+            public static string patternString = "";
+            public static string textString = "";
+            public static string archivoAbierto = "";
+        }
+        
+        //LAS SIGUIENTE FUINCION TE PERMITE CREAR UN NUEVO ARCHIVO
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            richTextBox.Show();
+            btnSave.Show();
+            btnSaveAs.Show();
+        }
+
+        //LA SIGUIENTE FUNCION GUARDA EL ARCHIVO CON UN NOMBRE ESTANDAR
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            /*StreamWriter File = new StreamWriter("Archivo.txt");
+            File.Write(richTextBox.Text);
+            File.Close();
+            richTextBox.Hide();*/
+            StreamWriter File = new StreamWriter(GlobalData.archivoAbierto);
+            File.Write(richTextBox.Text);
+            File.Close();
+
+        }
+
+        //SIRVE PARA ABRIR ARCHIVOS
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            label.Show();
+            txtArchive.Show();
+            btnSave.Show();
+        }
+
+        //LA SIGUIENTE FUNCION ABRE EL ARCHIVO QUE SE SOLICITA
+        private void txtArchive_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == Keys.Enter)
+            {
+                /*StreamReader leer = new StreamReader(txtArchive.Text);
+                string linea;
+                try
+                {
+                    linea = leer.ReadLine();
+                    while (linea != null)
+                    {
+                        if (leer.EndOfStream) 
+                        richTextBox.AppendText(linea + "\n");
+                        linea = leer.ReadLine();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Error");
+                }*/
+                TextReader leer;
+                leer = new StreamReader(txtArchive.Text);
+                GlobalData.archivoAbierto = txtArchive.Text;
+                richTextBox.Text = (leer.ReadLine());
+                GlobalData.textString = richTextBox.Text;
+                MessageBox.Show(GlobalData.textString);
+                leer.Close();
+                richTextBox.Show();
+            }
+        }
+        
+        //LA SIGUIENTE FUNCION TE ABRE EL TEXTBOX PARA PONER EL NOMBRE DE COMO QUIERAS GUARDAR EL ARCHIVO
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            label2.Show();
+            txtSaveAsArchive.Show();
+            btnSave.Hide();
+            btnSaveAs.Hide();
+        }
+
+        //LA SIGUIENTE FUNCION TE PERMITE GUARDAR EL ARCHIVO CON EL NOMBRE QUE DESEES
+        private void txtSaveAsArchive_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                StreamWriter File = new StreamWriter(txtSaveAsArchive.Text);
+                File.Write(richTextBox.Text);
+                File.Close();
+                richTextBox.Hide();
+                richTextBox.Hide();
+                label2.Hide();
+                txtSaveAsArchive.Hide();
+            }
+            
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            richTextBox.Hide();
+            btnSave.Hide();
+            btnSaveAs.Hide();
+        }
+
+        public int BoyerMoore(string myString)
+        {
+            GlobalData.patternString = myString;
+            GlobalData.pattern = myString.Length;
+
+            GlobalData.text = GlobalData.textString.Length;
+
+            int charcount = 0;
+            char[] siguiente;
+            char[] siguienteint;
+            siguiente = myString.ToCharArray();
+            for(int i = 1; i < siguiente.Length; i++)
+            {
+                charcount = i;
+            }
+            siguiente[charcount] = '0';
+            myString = new string(siguiente);
+            string a;
+            int k = GlobalData.pattern-1;
+            int j = GlobalData.pattern-1;
+            while (k <= GlobalData.text && j >= 1)
+            {
+                if (GlobalData.textString[k - (GlobalData.pattern - j - 1)] == GlobalData.patternString[j])
+                {
+                    j--;
+                }
+                else
+                {
+                    a = GlobalData.textString[k - (GlobalData.pattern - j - 1)].ToString();
+                    k = k + (GlobalData.pattern-1 - myString.IndexOf(a));
+                    j = GlobalData.pattern-1;
+                }
+            }
+
+            if (j == 0)
+            {
+                MessageBox.Show("La palabra se encuentra en el archivo");
+            }
+            else
+            {
+                MessageBox.Show("La palabra no se encuentra en el archivo");
+            }
+
+            return k;
+        }
+
+        private void txtSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == Keys.Enter)
+            {
+                string myString = txtSearch.Text;
+                BoyerMoore(myString);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            label3.Show();
+            txtSearch.Show();
+            cbSensible.Show();
+        }
+    }
+}
